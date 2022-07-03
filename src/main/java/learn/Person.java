e42eb7f1c0f777a790ca433e37eac63ff5e4eb4e
mikedr <derosamiguel@gmail.com>
2022-07-03 19:27:45 -0300

    First commit

---
+++ src/main/java/learn/Person.java
@@ -0,0 +1,31 @@
+package learn;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+	@Value("${boss.name}")
+	private String name;
+
+	@Value("${boss.lastName}")
+	private String lastName;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
